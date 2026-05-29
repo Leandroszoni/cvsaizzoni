641f4c77198d055bb9c0330ca6ca9a5e70bd0e10
Leandro Saiz Zoni <leandro@localhost>
2026-05-29 19:49:41 -0300

    Ajuste de paleta y contraste: fondo más oscuro y texto legible

--- cvSaizZoni/src/main.jsx
+++ cvSaizZoni/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
